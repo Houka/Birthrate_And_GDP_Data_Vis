616ee1237e37f68c9e89812cfbe7c618f40dd232
Houka <cl795@cornell.edu>
2017-03-04 15:27:54 -0500

    Add desc of circle graphs

--- circleGraphs.js
+++ circleGraphs.js
@@ -1,1 +1,5 @@
 // js for the circle graphs
+//
+// - gdp per cap would be size of circles, sqrt scale
+// - birth rate would be color, 7-10 discrete colors
+// - big circles to contain continents for organization purposes
