027d453080bca67ae5f22542ee69aefc1751746e
fawnwong <fawn.wong@live.com>
2017-03-07 13:06:30 -0500

    figured out continent translating

--- CircleGraph.js
+++ CircleGraph.js
@@ -4,15 +4,8 @@
 // - birth rate would be color, 7-10 discrete colors
 // - big circles to contain continents for organization purposes
 
-function mapLocationX(countryName){
-	var x = Math.random()*screenWidth;
-	return x;
-}
 
-function mapLocationY(countryName){
-	var y = Math.random()*screenHeight;
-	return y;
-}
+var birthRateColors =['#f7bba6','#ed8495','#e05286','#a73b8f','#6f2597','#511b75','#37114e'];
 
 function displayCombinedData(combinedData, nestedCombinedData){
 	nestedCombinedData = {
@@ -23,8 +16,6 @@ function displayCombinedData(combinedData, nestedCombinedData){
 	var svg = mainDiv.append("svg")
 	.attr("height", "100%")
 	.attr("width", "100%");
-
-	var birthRateColors =['#f7bba6','#ed8495','#e05286','#a73b8f','#6f2597','#511b75','#37114e'];
 
 	var birthRateExtent = d3.extent(combinedData, function(d) {	
 		var data = d.values[0];
@@ -48,73 +39,47 @@ function displayCombinedData(combinedData, nestedCombinedData){
 	var pack = d3.pack().size([screenWidth, screenHeight]);
 
 	var continent = nestedCombinedData;
-	// if (continent.name == "AF") { 
-		g = g.attr("transform", "translate("+screenWidth/-4+"0)"); // or whatever
-		continent = d3.hierarchy(continent)
-		.sum(function(d) {
-			return d.size;
-		});
 
-		pack(continent);
+	continent = d3.hierarchy(continent)
+	.sum(function(d) {
+		return d.size;
+	});
 
-		var node = g.selectAll(".node")
-		// .data(continent.descendants())
-		.data(continent.leaves())
-		.enter().append("g")
-		.attr("transform", function(d) {
-			/*if (d.data.name == "AF" || (d.parent) && d.parent.data.name == "AF") {
-				return "translate(" + (d.x + 300) + "," + d.y + ")"; 
-			}
-			else*/ 
-				return "translate(" + d.x + "," + d.y + ")"; 
-		});
+	pack(continent);
 
-		console.log(continent.leaves());
-		
-		node.append("circle")
-		.attr("r", function(d) {return d.r;})
-		.style("fill", function(d) {
-			if (d.data.birthrate){
-				return scaleBirthRate(+d.data.birthrate);
-			}
-			else
-				return "white";
-		})
-		.style("stroke", function(d) {
-			if (d.data.birthrate || d.data.name == "World") 
-				return "none";
-			else {
-				//console.log(d.data);
-				return "#ccc";
-			}
-		});
+	var node = g.selectAll(".node")
+	.data(continent.leaves())
+	.enter().append("g")
+	.attr("transform", function(d) {
+		// make this into a function, switch case on continent code
+		// translates all circles within the continent circle by the same amount
+		if ((d.parent.parent) && d.parent.parent.data.name == "AF") {
+			return "translate(" + (d.x + 300) + "," + d.y + ")"; 
+		}
+		else
+			return "translate(" + d.x + "," + d.y + ")"; 
+	});
 
-		node.append("title") 
-		.attr("text-anchor", "middle")
-		.attr("fill", "black")
-		.text(function(d) { 
-			return d.data.name + "\n" + format(d.value); });
+	console.log(continent.leaves());
 
+	node.append("circle")
+	.attr("r", function(d) {return d.r;})
+	.style("fill", function(d) {
+		if (d.data.birthrate){
+			return scaleBirthRate(+d.data.birthrate);
+		}
+		else {
+			return "white";
+		}
+	});
 
-		var node = g.selectAll(".node")
-		// .data(continent.descendants())
-		.filter(function(d) { d.data.region == "EU" })
-		.data(continent.leaves())
-		.enter().append("g")
-		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	node.append("text") 
+	.attr("text-anchor", "middle")
+	.attr("fill", "black")
+	.text(function(d) { 
+		return d.data.name; 
+	});
 
-			/*var br = scaleBirthRate(+d.data.birthrate);
-			var gdp = scaleGDP(+d.data.size);*/
-
-		node.append("circle")
-			.attr("r", function(d) {return d.r;})
-			.style("fill", function(d) { return scaleBirthRate(+d.data.birthrate); });
-
-		node.append("title") 
-		.attr("text-anchor", "middle")
-		.attr("fill", "black")
-		.text(function(d) { 
-			return d.data.name + "\n" + format(d.value); });
 
 	var legendSize = d3.scaleOrdinal()
 	.domain(birthRateExtent)
