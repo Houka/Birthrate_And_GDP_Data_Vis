ab9f6ed8337251599e03a28809837e352afda280
Houka <cl795@cornell.edu>
2017-03-06 21:50:10 -0500

    Changed countryToContinentMapping.csv to json and add country invalidation

--- circleGraphs.js
+++ circleGraphs.js
@@ -3,33 +3,6 @@
 // - gdp per cap would be size of circles, sqrt scale
 // - birth rate would be color, 7-10 discrete colors
 // - big circles to contain continents for organization purposes
-function displayBirthAndGDPData(birthData,GDPData,year){
-	// Code in here
-	var result = [];
-	for(var i = 0; i<birthData.length; i++){
-		var birthObj = birthData[i].values[0];
-		var GDPObj = GDPData[i].values[0];
-
-		var birthYear = birthObj[year];
-		var GDPYear = GDPObj[year];
-
-		var combinedObj = {
-			key:birthObj.country,
-			values:[
-				{
-					country: birthObj.country,
-					birthrate: birthYear,
-					gdp: GDPYear
-				}
-			]
-		}
-		console.log(result);
-		result = result.concat([combinedObj]);
-	}
-
-	return result;
-}
-
 function mapLocationX(countryName){
 	var x = Math.random()*screenWidth;
 	return x;
